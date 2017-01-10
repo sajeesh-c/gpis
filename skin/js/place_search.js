769c41ebc588de1968a73703fadc05f18c1fd63d
sajeeshc <sajeeshcheviry@gmail.com>
2017-01-10 21:54:56 +0530

    updated search type

--- skin/js/place_search.js
+++ skin/js/place_search.js
@@ -90,7 +90,7 @@ function initialize( Latitude, Longitude ) {
     var request = {
         location: center,
         radius: 2047,
-        types: ['Hospital']
+        types: ['food','hospital','health']
     };
 
     infowindow = new google.maps.InfoWindow();
